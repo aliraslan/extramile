61a840df5d2bd1bec5f5128e541ec373abcc676f
infix <40860821+infix@users.noreply.github.com>
2019-02-19 22:55:12 +0200

    Updated the index.ts and package.json and prepared them to be easily built and deployed. added build, install, dev, and clean scripts

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { UserResolver } from "./resolvers/UserResolver";
 import * as session from "express-session";
 import { TripResolver } from "./resolvers/TripResolver";
 import { FeedbackResolver } from "./resolvers/FeedbackResolver";
+import * as path from "path";
 
 @Resolver()
 export class ConnectionResolver {
@@ -17,8 +18,10 @@ export class ConnectionResolver {
   }
 }
 
+// TODO check if parcel or webpack can bundle the backend into a single file.
+
 const main: any = async () => {
-  await createConnection();
+  await createConnection(process.env.NODE_ENV == "production" ? "prod" : "default");
   // TODO (IMPORTANT) add lazy column and table joining depending
   //  on the query i.e. if a `current` query requests the trips
   //  automatically join the trips column. instead of proactively doing it.
@@ -34,11 +37,12 @@ const main: any = async () => {
 
   const apolloServer = new ApolloServer({
     schema,
-    playground: true,
+    playground: process.env.NODE_ENV != 'production',
     context: ({ req }: any) => ({ req })
   });
 
   const app = Express();
+  // TODO (important, before deploying) change this to redis
   app.use(
     session({
       secret: "shh",
@@ -50,13 +54,41 @@ const main: any = async () => {
     })
   );
 
-  apolloServer.applyMiddleware({ app, cors: { credentials: true } });
-
-  app.listen(4000, () => {
-    console.log(
-      `Server is running, GraphQL Playground available at http://localhost:4000/graphql`
-    );
+  apolloServer.applyMiddleware({
+    app,
+    cors: { credentials: true },
+    path: process.env.NODE_ENV == "production" ? "/" : ""
   });
+
+  // i genuinely hate myself for writing this piece of shit but it works
+  if (process.env.NODE_ENV == "production") {
+    // New Express app that will handle the static files.
+    const App = Express();
+
+    // let "child" app handle graphql
+    App.post("/graphql", app);
+
+    App.use(Express.static(path.resolve(__dirname, "public")));
+    App.get("*", (_, res) => {
+      res.sendFile(path.resolve(__dirname, "public", "index.html"));
+    });
+
+    App.listen(process.env.PORT || 4000, () => {
+      console.log(
+        `Server is running, GraphQL Endpoint available at http://localhost:4000${
+          apolloServer.graphqlPath
+          }`
+      );
+    });
+  } else {
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(
+        `Server is running, GraphQL Playground available at http://localhost:4000${
+          apolloServer.graphqlPath
+          }`
+      );
+    });
+  }
 };
 
 main();
