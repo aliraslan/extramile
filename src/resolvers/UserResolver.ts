3912e54bafb4083195cf9166d79eacf316b28f31
infix <40860821+infix@users.noreply.github.com>
2019-02-20 00:24:02 +0200

    replaced argon2 with bcryptjs

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,42 +1,44 @@
 import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { User } from "../entity/User";
-import * as argon2 from "argon2";
+import * as bcrypt from "bcryptjs";
 
 @Resolver()
 export class UserResolver {
-    @Query(() => User, {nullable: true})
-    async current(@Ctx() context: any): Promise<User | undefined> {
-        if (context.req.session.userId) {
-          const passenger = await User.findOne(context.req.session.userId, { relations: ["tickets", 'trips'] });
-            console.log(passenger);
-            return passenger;
-        } else {
-            return undefined;
-        }
+  @Query(() => User, { nullable: true })
+  async current(@Ctx() context: any): Promise<User | undefined> {
+    if (context.req.session.userId) {
+      const passenger = await User.findOne(context.req.session.userId, { relations: ["tickets", 'trips'] });
+      console.log(passenger);
+      return passenger;
+    } else {
+      return undefined;
+    }
+  }
+
+  @Mutation(() => User, { nullable: true })
+  async Login(@Arg("email") email: string,
+              @Arg("password") password: string,
+              @Ctx() ctx: any
+  ): Promise<User | null> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user || !await bcrypt.compare(password, user.password)) {
+      return null
     }
 
-    @Mutation(() => User)
-    async Login(@Arg("email") email: string,
-                @Arg("password") password: string,
-                @Ctx() ctx: any
-    ): Promise<User | null> {
-        const user = await User.findOne({where: {email}});
-        if (!user || !await argon2.verify(user.password, password)) {
-            return null
-        }
+    ctx.req.session.userId = user.id;
+    return user;
+  }
 
-        ctx.req.session.userId = user.id;
-        return user;
-    }
-
-    @Mutation(() => User)
-    async Register(@Arg("email") email: string,
-                   @Arg("firstName") firstName: string,
-                   @Arg("password") password: string
-    ): Promise<User> {
-        const hash = await argon2.hash(password);
-        return await User.create({
-            email, firstName, password: hash
-        }).save()
-    }
+  @Mutation(() => User)
+  async Register(@Arg("email") email: string,
+                 @Arg("firstName") firstName: string,
+                 @Arg("password") password: string
+  ): Promise<User> {
+    // const hash = await argon2.hash(password);
+    const hash = await bcrypt.hash(password, 2);
+    return await User.create({
+      email, firstName, password: hash
+    }).save()
+  }
 }
