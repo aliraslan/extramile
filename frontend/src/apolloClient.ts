f7ab77138b45321a8cb53799efa888fab70552e5
infix <40860821+infix@users.noreply.github.com>
2019-02-24 06:41:17 +0200

    disabled websockets

--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -1,9 +1,9 @@
 import { HttpLink, InMemoryCache } from "apollo-boost";
 import { ApolloClient } from "apollo-client";
-import { WebSocketLink } from 'apollo-link-ws';
-
-import { split } from 'apollo-link';
-import { getMainDefinition } from 'apollo-utilities';
+// import { WebSocketLink } from 'apollo-link-ws';
+//
+// import { split } from 'apollo-link';
+// import { getMainDefinition } from 'apollo-utilities';
 
 
 const httpLink = new HttpLink({
@@ -12,28 +12,28 @@ const httpLink = new HttpLink({
 });
 
 
-const wsLink = new WebSocketLink({
-  uri: process.env.REACT_APP_SERVER_URL_WS!,
-  options: {
-    reconnect: true,
-    reconnectionAttempts: 20,
-    connectionCallback: ((error, result) => {
-      console.log(`ERROR ${JSON.stringify(error)}`)
-    })
-  }
-});
+// const wsLink = new WebSocketLink({
+//   uri: process.env.REACT_APP_SERVER_URL_WS!,
+//   options: {
+//     reconnect: true,
+//     reconnectionAttempts: 20,
+//     connectionCallback: ((error, result) => {
+//       console.log(`ERROR ${JSON.stringify(error)}`)
+//     })
+//   }
+// });
 
-const link = split(
-  // split based on operation type
-  ({ query }) => {
-    // @ts-ignore
-    const { kind, operation } = getMainDefinition(query);
-    return kind === 'OperationDefinition' && operation === 'subscription';
-  },
-  wsLink,
-  httpLink,
-);
+// const link = split(
+//   // split based on operation type
+//   ({ query }) => {
+//     // @ts-ignore
+//     const { kind, operation } = getMainDefinition(query);
+//     return kind === 'OperationDefinition' && operation === 'subscription';
+//   },
+//   // wsLink,
+//   httpLink,
+// );
 export const client = new ApolloClient({
-  link,
+  link: httpLink,
   cache: new InMemoryCache()
 });
