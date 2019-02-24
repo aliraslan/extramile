1fa0e839824d14fb93bdda8831bc8c6326d190d7
Raslan <alinour1998@gmail.com>
2019-02-24 17:10:18 +0200

    Added PWA capabilities

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { DrawerView } from "./Drawer";
 import { MapView } from "./Map";
 import { EditProfile } from "./EditProfile";
+import { AuthorizedRoute } from "./CheckLoggedIn";
 
 const LoginMutation = gql`
   mutation LoginMutation($email: String!, $password: String!) {
@@ -23,71 +24,77 @@ export const Login = () => {
   const [password, setPassword] = useState("");
 
   return (
-    <Mutation mutation={LoginMutation}>
-      {(login, { data, loading, error }) => {
-        if (data) {
-          // you're now logged in!
-          return (
-            <div>
-              <MapView />
-            </div>
-          ); // Pending change
-        }
+    <AuthorizedRoute to="/map">
+      <Mutation mutation={LoginMutation}>
+        {(login, { data, loading, error }) => {
+          if (data) {
+            // you're now logged in!
+            return (
+              <div>
+                <MapView />
+              </div>
+            ); // Pending change
+          }
 
-        return (
-          <div className="prontoView">
-            <Input
-              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-              placeholder="Username"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              style={{
-                position: "absolute",
-                top: "40%",
-                left: "15%",
-                width: "70vw"
-              }}
-            />
-            <Input
-              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-              placeholder="Password"
-              style={{
-                position: "absolute",
-                top: "50%",
-                left: "15%",
-                width: "70vw"
-              }}
-              type="password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-            <Button
-              type="primary"
-              onClick={() => login({ variables: { email, password } })}
-              style={{
-                position: "absolute",
-                top: "60%",
-                left: "15%",
-                width: "70vw"
-              }}
-            >
-              Login
-            </Button>
-            <Button
-              type="ghost"
-              style={{
-                position: "absolute",
-                top: "70%",
-                left: "15%",
-                color: "white",
-                width: "70vw"
-              }}
-            >
-              <Link to="/register">New here? Register!</Link>
-            </Button>
-          </div>
-        );
-      }}
-    </Mutation>
+          return (
+            <div className="prontoView">
+              <Input
+                prefix={
+                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="Username"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                style={{
+                  position: "absolute",
+                  top: "40%",
+                  left: "15%",
+                  width: "70vw"
+                }}
+              />
+              <Input
+                prefix={
+                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="Password"
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "15%",
+                  width: "70vw"
+                }}
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+              <Button
+                type="primary"
+                onClick={() => login({ variables: { email, password } })}
+                style={{
+                  position: "absolute",
+                  top: "60%",
+                  left: "15%",
+                  width: "70vw"
+                }}
+              >
+                Login
+              </Button>
+              <Button
+                type="ghost"
+                style={{
+                  position: "absolute",
+                  top: "70%",
+                  left: "15%",
+                  color: "white",
+                  width: "70vw"
+                }}
+              >
+                <Link to="/register">New here? Register!</Link>
+              </Button>
+            </div>
+          );
+        }}
+      </Mutation>
+    </AuthorizedRoute>
   );
 };
